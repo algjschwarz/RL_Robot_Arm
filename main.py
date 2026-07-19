bf8d1322f4bd47ab7a95e7afba776edc90626198
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-07-18 22:25:45 -0500

    Created network class

--- main.py
+++ main.py
@@ -1,23 +1,37 @@
 import gymnasium as gym
 import time
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import numpy as np
 
-env = gym.make("Pusher", render_mode="human")
 
-observation, info = env.reset()
+class Arm_Net(nn.Module):
+    def __init__(self, input_size, action_space_size):
+        super().__init__()
 
-print(observation)
+        self.input = nn.Linear(input_size, 128)
+        self.output = nn.Linear(128, action_space_size)
 
-episode_over = False
-total_reward = 0
+    def forward(self, input):
+        l1 = F.relu(self.input(input))
+        l2 = self.output(l1)
+        return l2
 
-while not episode_over:
-    print(env.action_space)
-    action = env.action_space.sample()
-    observation, reward, terminated, truncated, info = env.step(action)
-    
-    time.sleep(.5)
-    total_reward += reward
-    episode_over = terminated or truncated
+def main():
+    env = gym.make("Pusher", render_mode="human")
+    observation, info = env.reset()
+    episode_over = False
 
-print(total_reward)
-env.close()
+    while not episode_over:
+        action = env.action_space.sample()
+        action = np.zeros(7)
+        action[0] = 1
+        observation, reward, terminated, truncated, info = env.step(action)
+        print(f"observation: {observation}, reward: {reward}, info: {info}")
+        time.sleep(.5)
+        total_reward += reward
+        episode_over = terminated or truncated
+
+if __name__ == "__main__":
+    main()

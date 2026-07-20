5b9e8c681c298d80fc9ddf1e69603cc173d543ad
Alex Three <algjschwarz@gmail.com>
2026-07-20 17:22:48 -0500

    Began Sampling of distributions for next action

--- main.py
+++ main.py
@@ -12,25 +12,31 @@ class Arm_Net(nn.Module):
 
         self.input = nn.Linear(input_size, 128)
         self.output = nn.Linear(128, action_space_size)
+        self.SD = nn.parameter(torch.zeros(action_space_size))
 
     def forward(self, input):
         l1 = F.relu(self.input(input))
-        l2 = self.output(l1)
-        return l2
+        means = self.output(l1)
+        return means, torch.exp(self.SD)
 
 def main():
     env = gym.make("Pusher", render_mode="human")
     observation, info = env.reset()
+    log_probabilities = []
+    rewards = []
     episode_over = False
+    net = Arm_Net(env.observation_space.shape[0], env.action_space.shape[0])
 
     while not episode_over:
-        action = env.action_space.sample()
-        action = np.zeros(7)
-        action[0] = 1
+        means, SD = net(observation)
+        sample = torch.normal(means, SD)
+        print(sample)
+
         observation, reward, terminated, truncated, info = env.step(action)
         print(f"observation: {observation}, reward: {reward}, info: {info}")
         time.sleep(.5)
-        total_reward += reward
+        log_probabilities.append(torch.tensor(0.0)) 
+        rewards.append(reward)
         episode_over = terminated or truncated
 
 if __name__ == "__main__":

a09930e9acc3f28b7be713bbfb88ac8d3ec92b46
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-07-21 00:36:45 -0500

    Added sampling

--- main.py
+++ main.py
@@ -12,32 +12,37 @@ class Arm_Net(nn.Module):
 
         self.input = nn.Linear(input_size, 128)
         self.output = nn.Linear(128, action_space_size)
-        self.SD = nn.parameter(torch.zeros(action_space_size))
+        self.SD = nn.Parameter(torch.zeros(action_space_size))
 
     def forward(self, input):
         l1 = F.relu(self.input(input))
         means = self.output(l1)
         return means, torch.exp(self.SD)
 
-def main():
-    env = gym.make("Pusher", render_mode="human")
+def run_episode(net, env):
     observation, info = env.reset()
     log_probabilities = []
     rewards = []
     episode_over = False
-    net = Arm_Net(env.observation_space.shape[0], env.action_space.shape[0])
-
     while not episode_over:
+        observation = torch.tensor(observation, dtype=torch.float32)
         means, SD = net(observation)
-        sample = torch.normal(means, SD)
-        print(sample)
+        dist = torch.distributions.Normal(means, SD)
+        sample = dist.sample()
+        log_prob = dist.log_prob(sample)
+        action = sample.detach().numpy()
 
         observation, reward, terminated, truncated, info = env.step(action)
-        print(f"observation: {observation}, reward: {reward}, info: {info}")
-        time.sleep(.5)
-        log_probabilities.append(torch.tensor(0.0)) 
+        log_probabilities.append(log_prob.sum()) 
         rewards.append(reward)
         episode_over = terminated or truncated
+        time.sleep(.5)
+
+def main():
+    env = gym.make("Pusher", render_mode="human")
+    net = Arm_Net(env.observation_space.shape[0], env.action_space.shape[0])
+    run_episode(net=net, env=env)
+    
 
 if __name__ == "__main__":
     main()

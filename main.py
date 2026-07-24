4d276551e4ca21ffef1246d137a42a79dbe1ea67
algjschwarz <149257078+alstryka@users.noreply.github.com>
2026-07-24 13:26:34 -0500

    Added a critic network

--- main.py
+++ main.py
@@ -9,18 +9,24 @@ import numpy as np
 class Arm_Net(nn.Module):
     def __init__(self, input_size, action_space_size):
         super().__init__()
-
         self.input = nn.Linear(input_size, 128)
         self.output = nn.Linear(128, action_space_size)
         self.SD = nn.Parameter(torch.zeros(action_space_size))
-
     def forward(self, input):
         l1 = F.relu(self.input(input))
         means = self.output(l1)
         return means, torch.exp(self.SD)
 
-def run_episode(net, env) -> list:
-    observation, info = env.reset()
+class Critic(nn.Module):
+    def __init__(self, input_size):
+        super().__init__()
+        self.input = nn.Linear(input_size, 32)
+        self.output = nn.Linear(32, 1)
+    def forward(self, input):
+        l1 = F.relu(self.input(input))
+        return self.output(l1)
+
+def run_episode(net, env, observation) -> list:
     log_probabilities = []
     rewards = []
     episode_over = False
@@ -39,7 +45,7 @@ def run_episode(net, env) -> list:
         #time.sleep(.001)
     return log_probabilities, rewards
 
-def accumulate_rewards(rewards, gamma=0.99):
+def accumulate_rewards(rewards, gamma=0.95):
     accumulated_rewards = [0] * len(rewards)
     running = 0
     for i in reversed(range(len(rewards))):
@@ -59,31 +65,58 @@ def calculate_mean_reward_for_timestep(episodes: list) -> list:
         mean_reward_for_timestep.append(mean)
     return mean_reward_for_timestep
 
+def run_episodes(episode_length, env, watch_env, net, critic, generation) -> list:
+    log_probabilities_episodes, rewards_episodes, predicted_returns = [], [], []
+    for j in range(episode_length):
+        observation, info = env.reset()
+        obs = torch.tensor(observation, dtype=torch.float32)
+        predicted_returns.append(critic(obs))
+        if 2950 < generation < 3000:
+            environment = watch_env
+        else:
+            environment = env
+        log_probabilities, rewards = run_episode(net=net, env=environment, observation=observation)
+        log_probabilities_episodes.append(log_probabilities)
+        rewards_episodes.append(rewards)
+    return log_probabilities_episodes, rewards_episodes, predicted_returns
+
 def main():
     env = gym.make("Pusher")
+    watch_env = gym.make("Pusher", render_mode="human")
     net = Arm_Net(env.observation_space.shape[0], env.action_space.shape[0])
+    critic = Critic(env.observation_space.shape[0])
     optimizer = torch.optim.Adam(net.parameters())
-    episode_length = 50
+    critic_optimizer = torch.optim.Adam(critic.parameters())
+    episode_length = 10
+    lowest_average = -500
+    generation_of_lowest_average = 0
     for i in range(1000):
-        print(f"Generation {i}")
-        log_probabilities_episodes, rewards_episodes = [], []
-        for j in range(episode_length):
-            log_probabilities, rewards = run_episode(net=net, env=env)
-            log_probabilities_episodes.append(log_probabilities)
-            rewards_episodes.append(rewards)
-        mean_reward_for_timestep = torch.stack(calculate_mean_reward_for_timestep(rewards_episodes))
+        log_probabilities_episodes, rewards_episodes, predicted_returns = run_episodes(episode_length, env, watch_env, net, critic, generation=i)
+        #mean_reward_for_timestep = torch.stack(calculate_mean_reward_for_timestep(rewards_episodes))
         loss = 0
+        critic_loss = 0
         for j in range(episode_length):
-            returns = torch.stack(accumulate_rewards(rewards_episodes[j]))
-            advantage = returns - mean_reward_for_timestep.detach()
+            returns = torch.stack(accumulate_rewards(rewards_episodes[j]))[0]
+            expected_return = predicted_returns[j]
+            advantage = returns - expected_return.detach()
             loss = loss - (torch.stack(log_probabilities_episodes[j]) * advantage).sum()
+            critic_loss = critic_loss + (expected_return - returns)**2
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-        avg_total = sum(sum(r).item() for r in rewards_episodes) / episode_length
-        print(f"Generation {i}  avg reward {avg_total:.1f}")
+        critic_optimizer.zero_grad()
+        critic_loss.backward()
+        critic_optimizer.step()
 
+        avg_total = sum(sum(r).item() for r in rewards_episodes) / episode_length
+        if avg_total > lowest_average:
+            generation_of_lowest_average = i
+            lowest_average = avg_total
+
+        print(f"Generation {i}  avg reward {avg_total:.1f}  Critic Loss {critic_loss}")
+    print(f"Highest Average {lowest_average}, Generation of highest average {generation_of_lowest_average}")
     env.close()
+    watch_env.close()
 
 if __name__ == "__main__":
     main()
